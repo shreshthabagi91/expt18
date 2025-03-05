886e68d47c1eab850137240a7a815115673e4a14
shreshthabagi91 <klsvppcs23shreshthagb@gmail.com>
2025-03-05 16:52:21 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,4 +1,8 @@
-class Helloworld.java
-  {
-    System.out.println("Hello Guys")
-  }
+class Helloworld {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
